c9e7a3323ef852fe9049234b236177ae67966d73
Remi Ferrand <remi.ferrand@cc.in2p3.fr>
2017-03-28 10:52:57 +0200

    Add new parameter to override fooacl script path

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,6 +8,7 @@
 class fooacl (
   $fooacl_noop      = false,
   $acl_package_name = $::fooacl::params::acl_package_name,
+  $fooacl_script_path = $::fooacl::params::fooacl_script_path
 ) inherits ::fooacl::params {
 
   if $acl_package_name {
@@ -18,25 +19,25 @@ class fooacl (
 
   $notify = $fooacl_noop ? {
     true  => undef,
-    false => Exec['/usr/local/sbin/fooacl'],
+    false => Exec[$fooacl_script_path]
   }
 
   # Main script, to apply ACLs when the configuration changes
-  exec { '/usr/local/sbin/fooacl':
+  exec { $fooacl_script_path:
     refreshonly => true,
     require     => Package[$acl_package_name],
   }
-  concat { '/usr/local/sbin/fooacl':
+  concat { $fooacl_script_path:
     mode   => '0755',
     notify => $notify,
   }
   concat::fragment { 'fooacl-header':
-    target  => '/usr/local/sbin/fooacl',
+    target  => $fooacl_script_path,
     order   => 10,
     content => template("${module_name}/10.erb"),
   }
   concat::fragment { 'fooacl-footer':
-    target  => '/usr/local/sbin/fooacl',
+    target  => $fooacl_script_path,
     order   => 30,
     content => template("${module_name}/30.erb"),
   }
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,6 +3,9 @@
 # Per-platform parameters for fooacl class
 #
 class fooacl::params {
+
+  $fooacl_script_path = '/usr/local/sbin/fooacl'
+
   case $::osfamily {
     'gentoo': {
       $acl_package_name = 'sys-apps/acl'
